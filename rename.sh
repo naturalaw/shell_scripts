4f9a48d6cadf3361b66eb73c051501a04c4815e9
danny <xizhengbing@gmail.com>
2012-06-07 03:30:45 +0800

    This is 6th ver

--- rename.sh
+++ rename.sh
@@ -1,11 +1,14 @@
 #/bin/bash
 # by Daniel Chow
+if [ $PWD != $HOME/pic ];then 
+cd ~/pic
+fi
 begin=$1
 end=$2
 prefix=$3
 if [ $# == 3 -a $1 -lt $2 -a -f $1.jpg -a -f $2.jpg ];then {
 	total_pages=`expr $end - $begin + 1`
-	echo -e "从第 $begin 到第 $end 重命名,共 $total_pages 个\n"
+	echo -e "当前目录是$PWD\n从第 $begin 到第 $end 重命名,共 $total_pages 个\n"
 
 	for page in `seq $begin $end`;do 
 			mv $page.jpg $prefix$page.jpg -v
@@ -13,7 +16,7 @@ if [ $# == 3 -a $1 -lt $2 -a -f $1.jpg -a -f $2.jpg ];then {
 }
 else 
 	echo -e "使用方法:\n
-	./re 开始页码 结束页码 前缀\n
-	如 ./re 1 20 aa 表示从1到20页的文件名添加前缀aa\n
+	re 开始页码 结束页码 前缀\n
+	如 re 1 20 aa 表示从1到20页的文件名添加前缀aa\n
 	注意：开始页码要小于结束页码"
 fi
