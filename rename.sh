a809777aa7b19d1525a130ba3306849b00614f8e
danny <xizhengbing@gmail.com>
2012-06-07 03:29:39 +0800

    This is 2nd ver

--- rename.sh
+++ rename.sh
@@ -3,7 +3,6 @@ unset old
 unset new
 ext=.jpg
 # 当全部图片要改名时才能使用这样的MD5计算
-md5sum *.jpg >old.txt
 if [ $# == 2 -a $1 -lt $2 ];then {
 	begin=$1
 	end=$2
@@ -16,28 +15,31 @@ if [ $# == 2 -a $1 -lt $2 ];then {
 		#echo $old;
 		new=`expr $sum - $old`
 		#echo $new
-		if [ $old -le $mid -a $old -ne $new ];then
+		if [ $old -le $mid -a $old -ne $new ];then {
 			echo $old' -->' $new 
-			mv $new$ext var$ext -v
-			mv $old$ext $new$ext -v
-			mv var$ext $old$ext -v
-		else
-			echo -e "$old = $new 文件无需重命名" 
+			echo $new' -->' $old
+			mv $new$ext var$ext 
+			mv $old$ext $new$ext 
+			mv var$ext $old$ext 
+		}
+		else { 
+			if [ $old == $new ];then
+			echo -e "$old is in the middle, no need to rename"
+			fi
+		}
 		fi
+		#md5sum $begin.jpg >>old.txt
 	done
-	md5sum *.jpg >new.txt
-	cat old.txt
-	echo -e "\n"
-	cat new.txt
-	sed -i "s/ .*//g" old.txt new.txt
-	tac new.txt>mid.txt
-	cat mid.txt >new.txt
-	rm mid.txt -f
-	diff old.txt new.txt && echo "重命名成功"
+#	md5sum *.jpg >new.txt
+#	sed -i "s/ .*//g" old.txt new.txt
+#	tac new.txt>mid.txt
+#	cat mid.txt >new.txt
+#	rm mid.txt -f
+#	diff old.txt new.txt && echo "重命名成功"
 }
 else 
-echo -e "使用方法:\n
-rename.sh 开始页码 结束页码\n
-如 rename.sh 1 20 表示反转命名1到20页的文件\n
-注意：开始页码要小于结束页码"
+	echo -e "使用方法:\n
+	rename.sh 开始页码 结束页码\n
+	如 rename.sh 1 20 表示反转命名1到20页的文件\n
+	注意：开始页码要小于结束页码"
 fi
