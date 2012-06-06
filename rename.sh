852f91ef37a892d3aa254d369ddbfe176a232199
danny <xizhengbing@gmail.com>
2012-06-07 03:31:44 +0800

    This is 9th ver

--- rename.sh
+++ rename.sh
@@ -1,42 +1,39 @@
 #!/bin/bash
 # by Daniel Chow @speedup2010
-# v1 modified 2012-5-16 20:39
-# v2 modified 2012-5-16 20:57
-# v3 modified 2012-5-17 17:49
-# v4 modified 2012-5-17 18:00
-# v5 modified 2012-5-19 16:03
-# v6 modified 2012-5-19 17:51
-# v7 modified 2012-5-20 12:34
-# v8 modified 2012-5-21 19:51
-if [ $PWD != $HOME/pic ];then 
-cd ~/pic
-fi
-begin=$1
-end=$2
-prefix=$3
-minus=$4
-ext=.jpg
-if [ $1 -lt $2 -a -f $1$ext -a -f $2$ext ];then {
-	total_pages=`expr $end - $begin + 1`
-	echo -e "当前目录$PWD\n重命名:从 $begin 到 $end ,共 $total_pages 个\n"
-	for page in `seq $begin $end`;do 
-		if [ $# == 3 ];then
-			mv $page$ext $prefix$page$ext -v
-		elif [ $# == 4 ];then 
-			new=`expr $page - $minus`
-			mv $page$ext $prefix$new$ext -v
-		elif [ $# == 5 -a $5 == p ];then 
-			plus=$minus
-			new=`expr $page + $plus`
-			mv $page$ext $prefix$new$ext -v
-		fi
-	done
+# 批量命名文件
+# v9 2012年05月24日 星期四 19:53:03
+# 下一版本加入转化utf8,省去第二步
+help(){
+	script_name=$0
+	script_name=`echo ${script_name##*/}`
+	script_name=`echo ${script_name%%.sh}`
+	# 这里的sql只是一个文件名的作用
+	echo -e "\t用法: $script_name sql\n
+	运行之前，先进行两步操作\n
+	1、把数据库相应的表导出为excel，另存为以制表符为分隔的txt文档\n
+	2、打开刚刚所保存的txt文档，另存为UTF-8的格式，sql为文件名，保存目录为pic\n"
 }
+
+if [ $# = 1 -a -f $1.txt ];then {
+	_dir=~/pic
+	sql=$1
+	TMP=tmp.txt
+	[ $PWD == $_dir ] || cd $_dir
+	[ -f $TMP.txt ] && rm $TMP.txt
+	grep -o '[0-9]\{8\}\.jpg.*是' $sql.txt | awk '$2!=0{print $2"\t"$1};$2==0{print $3"\t"$1}' | sort -g >>$TMP.txt
+	count=0
+	for var in *.jpg;do 
+		page=${var%%.jpg}; 
+		fn=`sed -n "/^$page\t/p" $TMP.txt | awk '{print $2}'`;
+		mv $page.jpg $fn && count=`expr $count + 1`
+		rm $TMP.txt $sql.txt
+	done
+echo "rename $count files ok"
+}
+elif [ ! -f $1.txt ];then
+	echo -e "\nerror: $1.txt does no exist\n
+	please do step 1 & step 2, and then run speed later. \n"
+		help
 else 
-	echo -e "
-	re使用方法:\n
-	re 开始页 结束页 前缀 [页码与后图件号几位之差] [p]\n
-	如 re 9 20 aa 表示从9到20页的文件名添加前缀aa\n
-	如 re 9 20 aa 4 表示从9到20页的文件名减4并添加前缀aa,故9.jpg改为aa5.jpg\n
-	注意：1.没有p参数时，差<开始页<结束页\n\t      2.页码与图件号几位的差/和 及 p 是可选参数\n\t "    
+	help
 fi
